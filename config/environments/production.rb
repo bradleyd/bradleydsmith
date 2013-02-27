74f0ebc6fae486e046d078fa506cff93dc3df264
Brad Smith <bradleydsmith@gmail.com>
2013-02-27 16:35:20 -0500

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Bradleydsmith::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -50,7 +50,7 @@ Bradleydsmith::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'scratch.bradleydsmith.com' }
   # Enable threaded mode
   # config.threadsafe!
 
