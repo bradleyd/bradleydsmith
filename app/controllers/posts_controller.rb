d6d582b172111988a99d56ee61f73753c8314c5a
Brad Smith <bradleydsmith@gmail.com>
2013-02-27 10:36:24 -0500

    add tinymce

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(params[:post])
-    @comment = @post.comments.create(params[:comment])
+    #@comment = @post.comments.create(params[:comment])
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
