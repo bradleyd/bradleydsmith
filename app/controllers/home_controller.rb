0fca0f93dc56fa3e29324c5fd4952c732bda0bb9
Bradley Smith <bradleydsmith@gmail.com>
2014-03-28 21:31:02 -0600

    sort post DESC

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @posts  = Post.includes(:comments).paginate(:page => params[:page], :per_page => 5)
+    @posts  = Post.includes(:comments).order("created_at DESC").
+      paginate(:page => params[:page], :per_page => 5)
     @random = Post.last 
   end
 
